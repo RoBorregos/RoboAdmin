827e9fe647f243fbbc09b3b8674205c8fa1bd804
Jonatandlr <A01735993@tec.mx>
2023-11-05 00:24:50 -0600

    imagesUpload2.0

---
+++ src/pages/images.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
+import { useState } from "react";
+import ImageDrop from "rbrgs/components/imageDrop";
+import ImagePreview from "rbrgs/components/imagePreview";
+
+const ImagesPage = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [croppedImage, setCroppedImage] = useState<string | null>(null);
+
+  const handleFileSelect = (file: File) => {
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-10">
+      <ImageDrop handleFileSelect={handleFileSelect} />
+
+      {selectedFile && (
+        <div>
+          <ImagePreview image={URL.createObjectURL(selectedFile)} onFinishedCropping={(image) => setCroppedImage(image)} desiredWidth={100} desiredHeight={200} />
+        </div>
+      )}
+      
+      {croppedImage && <img src={croppedImage} alt="" />}
+    </div>
+  );
+};
+
+export default ImagesPage;
